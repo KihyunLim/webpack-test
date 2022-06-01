ca8de76b0aac6c848f8ee23dfe3f2e79405c2448
khlim <khlim@home.com>
2022-06-01 11:40:43 +0900

    Output Management ~

--- src/index.js
+++ src/index.js
@@ -1,25 +1,9 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
-import Notes from './data.csv';
-import json from './data.json5';
 
 function component() {
   const element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  const myIcon = new Image();
-  myIcon.src = Icon;
-
-  element.appendChild(myIcon);
-
-  console.log(Data);
-  console.log(Notes);
-  console.log(json.title);
-  console.log(json.owner.name);
 
   return element;
 }
