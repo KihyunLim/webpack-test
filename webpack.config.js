7e6cb753713a2192a63fef0c05397bb13feccbbb
khlim <khlim@home.com>
2022-06-06 14:31:49 +0900

    Caching ~ @Output Filenames

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,15 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
-  entry: {
-    index: './src/index.js',
-  },
+  entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Caching',
+    }),
+  ],
   output: {
-    filename: '[name].bundle.js',
+    filename: '[name].[contenthash].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
     publicPath: '/',
