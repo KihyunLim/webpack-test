7040874f5517fd65c0bbaab57315c06b2b8c128d
khlim <khlim@home.com>
2022-06-12 13:56:40 +0900

    Production

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import { cube } from './math';
 
+if (process.env.NODE_ENV !== 'production') {
+  console.log(process.env.NODE_ENV, ' Looks like we are in development mode!');
+} else {
+  console.log('prd!!');
+}
+
 function component() {
   const element = document.createElement('pre');
 
