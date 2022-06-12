0bfe4b5fde27251c02df3692ac21df53a5c62179
khlim <khlim@home.com>
2022-06-12 15:24:50 +0900

    Shimming ~@Global Exports

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import _ from 'lodash';
+const {file, parse} = require('./globals');
 
 function component() {
   const element = document.createElement('div');
 
-  // Lodash, currently included via a script, is required for this line to work
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = join(['Hello', 'webpack'], ' ');
+
+  this.alert("Hmmm, this probably isn't a great idea...");
+
+  console.log(file);
+  parse();
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,26 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: require.resolve('./src/index.js'),
+        use: 'imports-loader?wrapper=window',
+      },
+      {
+        test: require.resolve('./src/globals.js'),
+        use: 'exports-loader?type=commonjs&exports=file,multiple|helpers.parse|parse',
+      },
+    ],
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      // _: 'lodash',
+      join: ['lodash', 'join'],
+    }),
+  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
